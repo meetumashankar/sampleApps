76862c5b19c2f8c29407d27adb3765ac83b4f10a
Umashankar Rathina Kumar <uma@192-168-1-107.tpgi.com.au>
2019-06-06 19:39:41 +1000

    Updated with three URI functions

---
+++ basicApp/src/dbFile.js
@@ -0,0 +1,14 @@
+module.exports = {
+name01:  [{
+  name: "umashankar",
+  age:  40,
+  profession: "IT"}],
+name02: [{
+  name: "Divya",
+  age:  32,
+  profession: "housewife"}],
+name03: [{
+  name:   "vilohith",
+  age:  10,
+  profesion: "Student"}]
+}
--- basicApp/src/index.js
+++ basicApp/src/index.js
@@ -1,8 +1,28 @@
+//import dbFile from './dbFile';
+
 var express = require('express');
 var app = express();
 app.listen(9090, () => {console.log("Service running in port 9090")});
-app.get("/basicApp", (req, res, next) => {
-  res.json('The names are '["Uma", "Shankar", "Divya", "Vilo"]);
-  res.json('success message')
+
+app.get("/basicApp/list_1", (req, res, next) => {
+  //res.json('The names are '["Uma", "Shankar", "Divya", "Vilo"]);
+  var msg = require('./dbFile.js')
+  res.json(msg.name01)
+  console.log("response list_1 sent")
+});
+
+app.get("/basicApp/list_2", (req, res, next) => {
+  //res.json('The names are '["Uma", "Shankar", "Divya", "Vilo"]);
+  var msg = require('./dbFile.js')
+  res.json(msg.name02)
+  console.log("response list_2 sent")
+});
+
+app.get("/basicApp/list_3", (req, res, next) => {
+  //res.json('The names are '["Uma", "Shankar", "Divya", "Vilo"]);
+  var msg = require('./dbFile.js')
+  res.json(msg.name03)
+  console.log("response list_3 sent")
 }
+
 );
