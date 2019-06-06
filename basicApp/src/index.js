f83966d208334b8630988af7f7eb611208b9de21
Umashankar Rathina Kumar <uma@192-168-1-107.tpgi.com.au>
2019-06-06 19:06:53 +1000

    gitHub Commit

--- basicApp/src/index.js
+++ basicApp/src/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 app.listen(9090, () => {console.log("Service running in port 9090")});
 app.get("/basicApp", (req, res, next) => {
-  res.json(["Uma", "Shankar", "Divya", "Vilo"]);
+  res.json('The names are '["Uma", "Shankar", "Divya", "Vilo"]);
+  res.json('success message')
 }
 );
